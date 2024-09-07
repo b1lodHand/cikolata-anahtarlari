9464feb6db7d784ad54244c2d714f2b2a5ba44af
absence <usturakeserway@gmail.com>
2024-09-07 12:30:49 +0300

    initial commit

---
+++ main/cikolata_anahtarlari/km/src/main.cpp
@@ -0,0 +1,17 @@
+#include <ntifs.h>
+
+void debug_print(PCSTR text)
+{
+#ifndef DEBUG
+	UNREFERENCED_PARAMETER(text);
+#endif
+
+	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
+}
+
+NTSTATUS DriverEntry()
+{
+	debug_print("[+] Entry to kernel.");
+
+	return STATUS_SUCCESS;
+}
---
+++ main/cikolata_anahtarlari/um/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "heyo\n";
+	std::cin.get();
+
+	return 0;
+}
